936bfa63e97a088492bdc1a8ca52655143a68076
Rod Guillaume <rodguillaume@tuta.io>
2019-03-13 00:10:59 +0100

    add 'wake up at' list in tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ModuleWithComponentFactories
+} from '@angular/core';
+
+import * as moment from 'moment';
+import {
+  Moment
+} from 'moment'; // type
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+
+  private readonly CYCLES = 6;
+
+  public timeSelected: Moment;
+  public cycleList: Moment[];
+
+  ngOnInit(): void {
+    this.timeSelected = moment();
+
+    this.cycleList = [];
+    for (let i = 1; i <= this.CYCLES; i++) {
+      this.cycleList.push(this.timeSelected.clone().add(i * 90, 'minutes'));
+    }
+
+    console.log(this.cycleList)
+  }
+}
